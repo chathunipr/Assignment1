c4dc935167550557457b3f09f4782f55ea9dccb8
chathunipr <chathuninperera@gmail.com>
2026-01-23 14:47:57 +0530

    Task3_Q02 solutions

---
+++ Task3/Q02.py
@@ -0,0 +1,14 @@
+cabinClass = input("Please enter your cabin class(LUX, A, B, or C): ")
+
+if cabinClass == "LUX":
+    print("Upper deck cabin with a balcony.")
+elif cabinClass == "A":
+    print("Above the car deck, equipped with a window.")
+elif cabinClass == "B":
+    print("Windowless cabin above the car deck.")
+elif cabinClass == "C":
+    print("Windowless cabin below the car deck.")
+else:
+    print("Invalid. Try Again")
+
+
