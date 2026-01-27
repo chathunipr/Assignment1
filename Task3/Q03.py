5fecae991fdad90be67f55bf4e1b28b0be447151
chathunipr <chathuninperera@gmail.com>
2026-01-27 13:16:57 +0530

    Q04 solutions and some updates

--- Task3/Q03.py
+++ Task3/Q03.py
@@ -1,3 +1,8 @@
+#Write a program that asks for the biological gender and hemoglobin value (g/l). 
+#The program the notifies the user if the hemoglobin value is low, normal or high.
+# A normal hemoglobin value for adult females is between 117-155 g/l.
+# A normal hemoglobin value for adult males is between 134-167 g/l.
+
 gender = input("Please enter your biological gender(F/M): ")
 hemoglobinVal = float(input("Enter your hemoglobin value: "))
 
